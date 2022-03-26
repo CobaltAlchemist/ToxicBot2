7d7ef4ddc4d9c6ea0c9f592e40fccd56363d1a19
Corey Senger <senge050@umn.edu>
2022-03-25 19:00:48 -0500

    Updated training with proper metrics

--- train_roberta.py
+++ train_roberta.py
@@ -31,32 +31,22 @@ class ToxicDatasetBert(tdata.Dataset):
 		#emojis = torch.tensor(row[self.emojis], dtype=torch.int32)
 		#hatemask = torch.tensor(~row[self.hatecols].isna())
 		return {**x, 'labels':toxicity}#, hate, hatemask, emojis)
-
-	def make_tokenizer(self, vocab_size=20_000):
-		tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
-		trainer = BpeTrainer(special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"], vocab_size=vocab_size)
-		tokenizer.pre_tokenizer = Whitespace()
-		tokenizer.train_from_iterator(self.dframe.comment_text, trainer)
-		return tokenizer
 		
-class Metrics:
-	def __init__(self):
-		self.acc = load_metric("accuracy", config_name='multilabel', average='macro')
-		self.f1 = load_metric("f1", config_name='multilabel', average='macro')
-		self.prec = load_metric("precision", config_name='multilabel', average='macro')
-		self.recall = load_metric("recall", config_name='multilabel', average='macro')
-
-	def __call__(self, eval_pred):
-		logits, labels = eval_pred
-		predictions = (labels > 0.).astype(np.int)
-		labels = (labels > 0.5).astype(np.int)
-		ret = {
-			'accuracy': self.acc.compute(predictions=predictions, references=labels),
-			'f1': self.f1.compute(predictions=predictions, references=labels, average='macro'),
-			'precision': self.prec.compute(predictions=predictions, references=labels, average='macro'),
-			'recall': self.recall.compute(predictions=predictions, references=labels, average='macro'),
-		}
-		return ret
+def all_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = (logits > 0.).astype(np.int64)
+    references = (labels > 0.5).astype(np.int64)
+    acc = load_metric("accuracy", config_name='multilabel', average='macro')
+    f1 = load_metric("f1", config_name='multilabel', average='macro')
+    prec = load_metric("precision", config_name='multilabel', average='macro')
+    recall = load_metric("recall", config_name='multilabel', average='macro')
+    ret = {
+        'accuracy': acc.compute(predictions=predictions, references=references),
+        'f1': f1.compute(predictions=predictions, references=references, average='macro'),
+        'precision': prec.compute(predictions=predictions, references=references, average='macro'),
+        'recall': recall.compute(predictions=predictions, references=references, average='macro'),
+    }
+    return ret
 
 if __name__ == "__main__":
 	tokenizer = RobertaTokenizer.from_pretrained('roberta-base', use_fast=False)
@@ -70,18 +60,21 @@ if __name__ == "__main__":
 	data_collator = DataCollatorWithPadding(tokenizer=tokenizer, max_length=256, padding='max_length', pad_to_multiple_of=8)
 	vset, tset = tdata.random_split(dset, [len(dset) // 10, len(dset) - len(dset) // 10])
 	
-	metric = Metrics()
-	
 	training_args = TrainingArguments(
-		output_dir="./models/Roberta Fine Tuned New",
+		output_dir="./models/Roberta1e",
 		learning_rate=5e-5,
 		per_device_train_batch_size=64,
-		per_device_eval_batch_size=1024,
-		num_train_epochs=3,
+		gradient_accumulation_steps=1,
+		per_device_eval_batch_size=512,
+		num_train_epochs=2,
+		save_strategy='epoch',
 		evaluation_strategy='epoch',
-		dataloader_num_workers=4,
+		dataloader_num_workers=8,
 		weight_decay=0.01,
+		bf16=True,
 		tf32=True,
+		optim='adamw_torch',
+		
 	)
 	trainer = Trainer(
 		model=model,
@@ -90,7 +83,7 @@ if __name__ == "__main__":
 		eval_dataset=vset,
 		tokenizer=tokenizer,
 		data_collator=data_collator,
-		compute_metrics=metric,
+		compute_metrics=all_metrics,
 	)
 	
-	trainer.train("models\Roberta Fine Tuned New\checkpoint-47500")
+	trainer.train()
